fcf20f6e19451707b5a3e6d7f04a9b48da359ec3
mob-sakai <sakai861104@gmail.com>
2022-02-18 04:52:37 +0900

    update

---
+++ Assets/UIAutoNaming.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAutoNaming
+{
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("UIAutoNaming/Naming")]
+    private static void Naming()
+    {
+        foreach (var text in Object.FindObjectsOfType<Text>())
+        {
+            var parent = text.transform.parent;
+            var selectable = parent.GetComponent<Selectable>();
+            if (selectable)
+            {
+                parent.name = string.Format("{0} - {1}", selectable.GetType().Name, text.text);
+                continue;
+            }
+
+            foreach (Transform tr in parent)
+            {
+                selectable = tr.GetComponent<Selectable>();
+                if (selectable)
+                {
+                    parent.name = string.Format("{0} - {1}", selectable.GetType().Name, text.text);
+                    break;
+                }
+            }
+
+        }
+    }
+#endif
+}
